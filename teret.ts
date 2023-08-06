4871ea769dde747bd22b01c4ffa0eeaa3e61d267
‪Tzuriel Weisberg‬‏ <tzubielo@gmail.com>
2023-08-06 12:49:45 +0300

    Create teret.ts

---
+++ teret.ts
@@ -0,0 +1,7 @@
+it('should update iframe src', () => {
+        component.onboardingStatus$ = of(OnboardingStatus.NotStarted);
+        component.isLoading = false;
+        fixture.detectChanges();
+        const videoIframe = () => fixture.debugElement.query(By.css('.test-video-iframe')).nativeElement;
+        expect(videoIframe().src).toBe(trialPageInformation.videoLink);
+    });
